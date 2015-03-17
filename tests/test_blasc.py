93349d259c568314bee25012ebf0248aeb6c12c6
Leonard Truong <leonardtruong@gmail.com>
2015-03-16 17:04:39 -0700

    update blasc test

--- tests/test_blasc.py
+++ tests/test_blasc.py
@@ -7,7 +7,34 @@ from ctree.frontend import get_ast
 import sys
 import ast
 import inspect
+import ctypes
+from ctypes import c_int, c_float, c_size_t
 
+import os
+from sys import platform as _platform
+
+if _platform == "linux" or _platform == "linux2":
+    ext = "so"
+elif _platform == "darwin":
+    ext = "dylib"
+
+_blaslib = ctypes.cdll.LoadLibrary("libcblas.{}".format(ext))
+
+
+def gemm(A, B, C, alpha, beta, m, n, k):
+    cblas_row_major = c_int(101)
+    no_trans = c_int(111)
+    m = c_int(int(m))
+    n = c_int(int(n))
+    k = c_int(int(k))
+    alpha = c_float(alpha)
+    beta = c_float(beta)
+    A_ptr = A.ctypes.data_as(ctypes.c_void_p)
+    B_ptr = B.ctypes.data_as(ctypes.c_void_p)
+    C_ptr = C.ctypes.data_as(ctypes.c_void_p)
+
+    _blaslib.cblas_sgemm(cblas_row_major, no_trans, no_trans, m, n, k,
+                         alpha, A_ptr, k, B_ptr, n, beta, C_ptr, n)
 
 def get_callable(tree, name, env):
     tree.body[0].decorator_list = []
@@ -21,9 +48,12 @@ def blasc(fn):
     symbol_table.update(frame.f_globals)
     symbol_table.update(frame.f_back.f_locals)
     symbol_table.update(frame.f_back.f_globals)
-    print(symbol_table)
     tree = get_ast(fn)
     def wrapped(*args, **kwargs):
+        print(args)
+        print([arg.id for arg in tree.body[0].args.args])
+        for index, arg in enumerate(tree.body[0].args.args):
+            symbol_table[arg.id] = args[index]
         # Process tree
         return get_callable(tree, tree.body[0].name, symbol_table)(*args, **kwargs)
     return wrapped
@@ -47,3 +77,10 @@ class TestBlasc(unittest.TestCase):
 
         self._check(fn(A, x, b), np.transpose(A).dot(x) - b)
 
+    def test_blase(self):
+        A = Array.rand(256, 256).astype(np.float32)
+        x = Array.rand(256, 256).astype(np.float32)
+        actual = Array.rand(256, 256).astype(np.float32)
+        gemm(A, x, actual, 1.0, 0.0, 256, 256, 256)
+
+        self._check(actual, np.dot(A, x))
